ce1b377fa937ded146cc7b9b618e54227e550b3b
Matthew Streeter <22773109+mjvstreeter@users.noreply.github.com>
2021-09-20 14:01:54 +0100

    Restarted repo

--- ParticlePusher3D.py
+++ ParticlePusher3D.py
@@ -1,10 +1,11 @@
 import numpy as np
-import scipy.constants as C
+from scipy.constants import c, m_e
+from scipy.constants import e as q_e
 np.seterr(divide='ignore')
 
 class ParticlePushObj:
     
-    def __init__(self,p_6d,EM_function,dtMin=0,dtMax=1,ppc=16,q=-1,m=1):
+    def __init__(self,p_6d,EM_function,dtMin=0,dtMax=1,ppc=16,q=-1,m=1,pushMethod='boris'):
         self.p_6d =  p_6d
         self.EM_function = EM_function
         self.dtMin=dtMin
@@ -12,6 +13,7 @@ class ParticlePushObj:
         self.ppc=ppc
         self.q=q
         self.m=m
+        self.pushMethod = pushMethod
         '''
         p_6d is a 6 by N array giving the 3 spatial and 3 momentum values for N particles
         p_6d[:,0] = x,y,z,p_x,p_y,p_z
@@ -26,8 +28,8 @@ class ParticlePushObj:
         B_mod = np.sqrt(np.sum(B_vec**2,axis=0))
 
         if np.max(B_mod)>0:
-            r = np.min(np.abs(np.divide(gamma*C.c*rqm,B_mod)))
-            dt = np.clip((2*np.pi*r)/C.c/(self.ppc),self.dtMin,self.dtMax)
+            r = np.min(np.abs(np.divide(gamma*c*rqm,B_mod)))
+            dt = np.clip((2*np.pi*r)/c/(self.ppc),self.dtMin,self.dtMax)
         else:
             dt = self.dtMax
         if np.isnan(dt) or np.isinf(dt):
@@ -57,8 +59,8 @@ class ParticlePushObj:
     def trackParticles(self,xLims=[[None]*2]*3,pLims=[[None]*2]*3,N_max=10000):
         ''' Tracks particles using the Boris push method
         '''
-        p_m = self.m*C.m_e
-        p_q = self.q*C.e
+        p_m = self.m*m_e
+        p_q = self.q*q_e
         rqm = p_m/p_q
         p_x = self.p_6d[0:3,:]
         p_px = self.p_6d[3:6,:]
@@ -71,19 +73,22 @@ class ParticlePushObj:
         while keepTracking:
             
             gamma = self._gamma_calc(p_px)
-            E_vec, B_vec = self.EM_function(p_x)
+            E_vec, B_vec = self.EM_function(t,p_x)
             
             dt = self._dt_calc(B_vec,gamma,rqm)
-            # offset velocity by -half step in first iteration
-            if n==0:
-                dt = -dt/2
-            else:
-                t = t + dt
+            t = t + dt
             t_list.append(t)
-            if n==0:
-                p_x_dump, p_px = self._boris_step(p_x,p_px,rqm,dt,E_vec,B_vec)
-            else:
-                p_x, p_px = self._boris_step(p_x,p_px,rqm,dt,E_vec,B_vec)
+            # offset velocity by -half step in first iteration
+            # if n==0:
+            #     dt = -dt/2
+            # else:
+            #     t = t + dt
+            # t_list.append(t)
+            # if n==0:
+            #     #p_x_dump, p_px = self._boris_step(p_x,p_px,rqm,dt,E_vec,B_vec)
+            #     p_x_dump, p_px = self._boris_step(p_x,p_px,rqm,dt,t)
+            # else:
+            p_x, p_px = self._boris_step(p_x,p_px,rqm,dt,t)
             p_6d_t.append(np.concatenate((p_x,p_px),axis=0))
             keepTracking = self._check_tracking_conditions(p_x,p_px,xLims,pLims,n,N_max)
             n=n+1
@@ -95,30 +100,52 @@ class ParticlePushObj:
         return t,p_6d_t
 
 
-    def _boris_step(self,p_x,p_px,rqm,dt,E_vec,B_vec):
+    def _boris_step(self,p_x,p_px,rqm,dt,t):
+        pushMethod = self.pushMethod
         # Boris method
         tem = 0.5 * dt / rqm
+
+        # half step move
+        g = np.tile(self._gamma_calc(p_px),(3,1))
+        p_x = p_x + p_px*c/g*dt/2
+
+        # get fields
+        E_vec, B_vec = self.EM_function(t,p_x)
+
         # half acceleration with E
-        p = p_px +E_vec*tem/C.c
+        p = p_px +E_vec*tem/c
+
         # compute inverse gamma and multiply by tem
-        
-        g_tem = np.tile(tem/self._gamma_calc(p),(3,1))
+        g = self._gamma_calc(p)
+    
+        if 'hc' in pushMethod:
+            g2 = 1+np.sum(p**2,axis=0)
+            
+            beta = tem * B_vec
+            beta2 = np.sum(beta**2,axis=0)
+            sigma =g2 - beta2
+           
+            beta_dot_u = np.sum(beta*p,axis=0)
+
+            g = np.sqrt(0.5*(sigma + np.sqrt(sigma**2 + 4.0 * (beta2 + beta_dot_u**2))))
+
+        g_tem = np.tile(tem/g,(3,1))
         B_vec = B_vec*g_tem
         
         # half rotation with B
-        pp = p + np.cross(p,B_vec,axis=0)
+        pp = p - np.cross(p,B_vec,axis=0)
     
         # Second half rotation
         B_vec = B_vec*2/(1+B_vec**2)
-        p =  p + np.cross(pp,B_vec,axis=0)
+        p =  p - np.cross(pp,B_vec,axis=0)
     
         # second half acceleration
-        p = p + E_vec*tem/C.c
+        p = p + E_vec*tem/c
         
-        # move particles
+        # final half move particles
         g = np.tile(self._gamma_calc(p),(3,1))
-        v = C.c*p/g
-        p_x =  p_x + v*dt
+        v = c*p/g
+        p_x =  p_x + v*dt/2
         p_px = p
         return p_x, p_px
 
